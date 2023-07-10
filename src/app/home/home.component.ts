8ed92f76d7311f81c7166faaecc16e6b543549f5
esrabrown <essrcskn@hotmail.com>
2023-07-10 00:34:46 -0500

    changed style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    document.body.addEventListener("mousemove", (evt: MouseEvent) => {
+      const mouseX = evt.clientX;
+      const mouseY = evt.clientY;
+  
+      gsap.set(".cursor", {
+        x: mouseX,
+        y: mouseY
+      });
+  
+      gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+        stagger: -0.1
+      });
+    });
+  }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    document.body.addEventListener("mousemove", (evt: MouseEvent) => {
+      const mouseX = evt.clientX;
+      const mouseY = evt.clientY;
+  
+      gsap.set(".cursor", {
+        x: mouseX,
+        y: mouseY
+      });
+  
+      gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+        stagger: -0.1
+      });
+    });
+  }
 }
