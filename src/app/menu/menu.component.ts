bc69ddd0d118658cec33944190b5e2e59ff585aa
esrabrown <essrcskn@hotmail.com>
2023-07-10 01:55:07 -0500

    added button

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component, AfterViewInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
-  menuItems: MenuItem[] = [
-    { label: 'Home', link: '/home' },
-    { label: 'About', link: '/about' },
-    { label: 'Skills', link: '/skills' },
-    { label: 'Projects', link:'/projects'},
-    { label: 'Contact', link: '/contact' }
-  ];
-}
+export class MenuComponent implements AfterViewInit {
+  @ViewChildren('item') items!: QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('indicator') indicator!: ElementRef<HTMLDivElement>;
 
-interface MenuItem {
-  label: string;
-  link: string;
+  constructor() { }
+
+  ngAfterViewInit() {
+    const indicator = this.indicator.nativeElement;
+    const items = this.items.toArray();
+
+    function handleIndicator(el: HTMLElement) {
+      items.forEach(item => {
+        item.nativeElement.classList.remove('is-active');
+        item.nativeElement.removeAttribute('style');
+      });
+
+      indicator.style.width = `${el.offsetWidth}px`;
+      indicator.style.left = `${el.offsetLeft}px`;
+      indicator.style.backgroundColor = el.getAttribute('active-color')!;
+
+      el.classList.add('is-active');
+      el.style.color = el.getAttribute('active-color')!;
+    }
+
+    items.forEach(item => {
+      item.nativeElement.addEventListener('click', (e) => { handleIndicator(e.target as HTMLElement) });
+      if (item.nativeElement.classList.contains('is-active')) {
+        handleIndicator(item.nativeElement);
+      }
+    });
+  }
 }
