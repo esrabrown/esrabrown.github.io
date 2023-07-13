c47948f38686775a345ea1dc32bfb24373e3f822
esrabrown <essrcskn@hotmail.com>
2023-07-13 00:11:53 -0500

    animated info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,57 @@ import { gsap } from 'gsap';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  message = [
+    'web development',
+    'traveling',
+    'coding',
+    'java, ...',
+    'good movie and popcorn',
+    'javascript, angular, ...',
+    'los aviones, la montaña, soñar',
+    'her şey :)'
+  ];
+  counterS = 0;
+  counterL = 0;
+  deleteS = false;
+  $text: any;
 
   constructor() { }
 
+
   ngOnInit(): void {
+    this.cacheElem();
+    this.type();
+  }
+
+  cacheElem() {
+    this.$text = document.querySelector('.text');
+  }
+
+  type() {
+    let message = this.message[this.counterS];
+    const that = this;
+    let speed = 0;
+
+    message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+    if (this.message[this.counterS] !== message && !this.deleteS) {
+      this.$text.textContent = message;
+      speed = 90;
+    } else {
+      this.deleteS = true;
+      speed = this.message[this.counterS] === message ? 1000 : 40;
+      this.$text.textContent = message;
+      if (message === '') {
+        this.deleteS = false;
+        this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+      }
+    }
+    setTimeout(function () {
+      that.type();
+    }, speed);
+  
+
+
     document.body.addEventListener("mousemove", (evt: MouseEvent) => {
       const mouseX = evt.clientX;
       const mouseY = evt.clientY;
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
-
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent  {
-
-  constructor() { }
-
-
-
+  
+  
 }
