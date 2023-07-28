e266b07c49d057b23337bb6110b03d6db2e7574e
esrabrown <essrcskn@hotmail.com>
2023-07-27 22:32:42 -0500

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -57,23 +57,6 @@ export class HomeComponent implements OnInit {
       that.type();
     }, speed);
   
-  
-
-    // document.body.addEventListener("mousemove", (evt: MouseEvent) => {
-    //   const mouseX = evt.clientX;
-    //   const mouseY = evt.clientY;
-  
-    //   gsap.set(".cursor", {
-    //     x: mouseX,
-    //     y: mouseY
-    //   });
-  
-    //   gsap.to(".shape", {
-    //     x: mouseX,
-    //     y: mouseY,
-    //     stagger: -0.1
-    //   });
-    // });
   }
 
   sendEmail(): void {
