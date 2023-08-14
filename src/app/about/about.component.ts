71b10b4c9a6efef421f9f10ec7668fff09370f89
esrabrown <essrcskn@hotmail.com>
2023-08-14 16:47:09 -0500

    final touches

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,25 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-
-
-  education: any[] = [
-  {
-    institution: 'Uludag University, Turkiye',
-    degree: 'Bachelor of Philosophy',
-    duration: '2010 - 2015'
-  },
-  {
-    institution: 'LaunchCode, St.Louis / MO',
-    degree: 'Certificate in Web Development',
-    duration: '2022-2023',
-    coursework: 'Full-Stack Web Development'
-  }
-];
-
-additionalCourses: string[] = [
-   'Computational Thinking for Problem Solving, University of Pennsylvania, Coursera — 2023',
-   'Java for Programmers Course, Codecademy — 2023'
-]
-}
+export class AboutComponent {}
